8be7a692a5d3323c220a09ba64330d992f7235ab
zaid ragab <zragab@lcs.on.ca>
2023-06-08 19:39:30 -0400

    got a basic ui with a random number attributes to a name that will soon be changeable

--- GenoratorOfRandomGroups/ContentView.swift
+++ GenoratorOfRandomGroups/ContentView.swift
@@ -1,21 +1,47 @@
-//
-//  ContentView.swift
-//  GenoratorOfRandomGroups
-//
-//  Created by Zaid Ragab on 2023-06-08.
-//
-
 import SwiftUI
 
+struct NumberEntry: Identifiable {
+    let id = UUID()
+    let name: String
+    let number: Int
+}
+
 struct ContentView: View {
+    @State private var upperBound: Int = 100
+    @State private var randomNumberEntries: [NumberEntry] = []
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(randomNumberEntries) { entry in
+                VStack(alignment: .leading) {
+                    Text("Name: \(entry.name)")
+                    Text("Number: \(entry.number)")
+                }
+            }
+
+            HStack {
+                Text("Upper Bound:")
+                TextField("Enter upper bound", value: $upperBound, formatter: NumberFormatter(), onCommit: {
+                    generateRandomNumber()
+                })
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding()
+            }
+            TextField("enter person name")
+
+            Button("Generate Random Number") {
+                generateRandomNumber()
+            }
+            .padding()
         }
         .padding()
+    }
+
+    private func generateRandomNumber() {
+        let randomName = "Name \(randomNumberEntries.count + 1)"
+        let randomNumber = Int.random(in: 1...upperBound)
+        let entry = NumberEntry(name: randomName, number: randomNumber)
+        randomNumberEntries.append(entry)
     }
 }
 
