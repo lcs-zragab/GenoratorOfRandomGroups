843231e698365bc4826eed03ee3aa4c8115bbddf
zaid ragab <zragab@lcs.on.ca>
2023-06-08 19:13:29 -0400

    Initial Commit

---
+++ GenoratorOfRandomGroups/GenoratorOfRandomGroupsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenoratorOfRandomGroupsApp.swift
+//  GenoratorOfRandomGroups
+//
+//  Created by Zaid Ragab on 2023-06-08.
+//
+
+import SwiftUI
+
+@main
+struct GenoratorOfRandomGroupsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
